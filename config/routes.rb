89691417689beb315a8767551ccadf7dea67a5b9
fred mac <mcfredmac@icloud.com>
2014-04-25 15:50:00 +0100

    added usrr sign in sign out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  root 'home#welcome'
   devise_for :users
 
   # Example of regular route:
