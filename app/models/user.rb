fb1de67a26b9dab28ba3332a324859b86cbb1f4a
fred mac <mcfredmac@icloud.com>
2014-04-25 11:34:03 +0100

    added user reg db test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  has_one :user_info 
+  has_one :user_profile
 end
