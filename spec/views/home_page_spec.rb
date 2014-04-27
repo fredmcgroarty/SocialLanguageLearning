6db82c07e5c8f42516268298d80ecec08088ab66
fred mac <mcfredmac@icloud.com>
2014-04-27 16:58:14 +0100

    a user can see the list of registered user on the homepage

--- spec/views/home_page_spec.rb
+++ spec/views/home_page_spec.rb
@@ -33,15 +33,30 @@ describe 'Home page' do
 	context "user has registered account and profile" do 
 			before(:each) do
 				@fred = create(:user)
-				create(:user_profile, user: @fred)
+				create(:user_profile)
+				@mark = create(:user)
+				create(:user_total_profile_1)
+				@tiff = create(:user)
+				create(:user_total_profile_2)
+				login_as @fred
+				visit '/'
 			end
 
 		it "sees a link to update their language info if empty" do 
-			login_as @fred
-			visit '/'
 			expect(page).not_to have_content("Sign up" && "Sign in")
 			expect(page).to have_content "Create your language profile"
 		end
+
+		it "can see an 'edit my profile' link" do 
+			expect(page).to have_link 'Settings'
+		end
+
+		it "can see a list of users' profiles" do 
+			expect(page).to have_content ('Mark M')
+			expect(page).to have_content('Tiff C')
+		end
+
+
 
 	end
 end
