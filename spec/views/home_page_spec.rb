da1916ec12a6c48dedebcabc62575977f69dffcb
fred mac <mcfredmac@icloud.com>
2014-04-27 21:11:11 +0100

    added bootstrap

--- spec/views/home_page_spec.rb
+++ spec/views/home_page_spec.rb
@@ -29,8 +29,24 @@ describe 'Home page' do
 			visit '/'
 			expect(page).to have_content("Sign out")
 		end
+	end
+end
 
-	context "user has registered account and profile" do 
+describe "personalised Home Page" do 
+
+	context "user has registered account but empty language profile" do 
+
+		it "sees a link to update their language info if empty" do 
+			expect(page).not_to have_content("Sign up" && "Sign in")
+			expect(page).to have_content "Create your language profile"
+		end
+	end
+
+		it "can see an 'edit my profile' link" do 
+			expect(page).to have_link 'Settings'
+		end
+
+	context "user has registered account and full language profile" do 
 			before(:each) do
 				@fred = create(:user)
 				create(:user_profile)
@@ -42,22 +58,10 @@ describe 'Home page' do
 				visit '/'
 			end
 
-		it "sees a link to update their language info if empty" do 
-			expect(page).not_to have_content("Sign up" && "Sign in")
-			expect(page).to have_content "Create your language profile"
-		end
-
-		it "can see an 'edit my profile' link" do 
-			expect(page).to have_link 'Settings'
-		end
-
 		it "can see a list of users' profiles" do 
 			expect(page).to have_content ('Mark M')
 			expect(page).to have_content('Tiff C')
 		end
 
-
-
 	end
-end
 end
