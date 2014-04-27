e6c9975ff32a5947b6746ad8c5877f28bb9a0916
fred mac <mcfredmac@icloud.com>
2014-04-28 00:28:30 +0100

    some tinkering

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -1,16 +1,28 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'editting users profile' do
+describe 'editting users profile' do
+  before(:each) do
+		create(:user_total_profile_1)
+		@tiff = create(:user)
+  end
 
-#   context "if no image is uploaded" do 
-#     before(:each) do
-#       login_as create(:user)
-#     end
+	context "security" do 
+		before(:each) do
+      login_as @tiff
+  		visit '/user_profiles/1/edit' 
+  		expect(current_path).to eq edit_user_profile_path(@tiff)
+  	end
 
-#   	it "a default picture will be the avatar" do 
-#   		visit '/user_profiles/1/edit'
-#   		# within ('.profile_pic')
-#   	end
-#   end
-# end
+		it "a user can't edit other users' profiles" do 
+			
+		end
+
+	end
+
+  context "if no image is uploaded" do 
+
+  	xit "Tiff can add a photo" do
+  	end
+  end
+end
 
