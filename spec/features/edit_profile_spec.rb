2b9b0118e3e9a2a974af9b77f2cf66221e4ede2c
fred mac <mcfredmac@icloud.com>
2014-04-28 10:22:02 +0100

    added hide url on edit profile security

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -2,21 +2,27 @@ require 'spec_helper'
 
 describe 'editting users profile' do
   before(:each) do
+		@mark = create(:user)
 		create(:user_total_profile_1)
-		@tiff = create(:user)
+  	@tiff = create(:user)
+		create(:user_total_profile_2)
   end
 
 	context "security" do 
 		before(:each) do
-      login_as @tiff
+      login_as @mark
   		visit '/user_profiles/1/edit' 
-  		expect(current_path).to eq edit_user_profile_path(@tiff)
+  		expect(current_path).to eq edit_user_profile_path(@mark)
   	end
 
-		it "a user can't edit other users' profiles" do 
-			
+		it "Mark can't edit Tiff's profile" do 
+			visit '/'
+			click_link 'Settings'
+			expect(current_path).to eq edit_user_profile_path(@mark)
+			visit '/user_profiles/2/edit'
+			save_and_open_page
+			expect(current_path).to eq('/') 
 		end
-
 	end
 
   context "if no image is uploaded" do 
