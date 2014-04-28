3422be3e78253869dcc06c43ba1845d458d1ccbd
BonoboUK <mark@superius.co.uk>
2014-04-28 11:27:38 +0100

    Added rMagick and delegations to user class

--- spec/features/delegation_feature_spec.rb
+++ spec/features/delegation_feature_spec.rb
@@ -2,14 +2,26 @@ require 'spec_helper'
 
 describe "Delegations" do
 	
-	context "When routing method calls" do
+	context "when routing method calls" do
 
-		it "Should route first_name to User_Profile" do
+		it "should route first_name to User_Profile" do
 			visit '/'
 			@fred = create(:user)
+			create(:user_profile)
 			expect(@fred.first_name).to eq "Fred"
 			expect(@fred.first_name).to eq @fred.user_profile.first_name
+		end
 
+		it "should route all applicable attributes" do
+			visit '/'
+			@fred = create(:user)
+			@mark = create(:user)
+			create(:user_total_profile_1)
+			expect(@mark.first_name).to eq "Mark"
+			expect(@mark.last_name).to eq "M"
+			expect(@mark.native_lang).to eq "English"
+			expect(@mark.first_lang).to eq "French"
+			expect(@mark.second_lang).to eq "Spanish"
 		end
 
 	end
