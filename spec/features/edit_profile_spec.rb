fab3e5d0429f99224dacb6049a269e7df7248d3e
RuslanVikhor <tokvbok@yahoo.co.uk>
2014-04-29 12:05:45 +0100

    password change aded to edit page

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -1,52 +1,52 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'editting users profile' do
-#   before(:each) do
-# 		@mark = create(:user)
-# 		create(:user_total_profile_1)
-#   	@tiff = create(:user)
-# 		create(:user_total_profile_2)
-#   end
+describe 'editting users profile' do
+  before(:each) do
+		@mark = create(:user)
+		create(:user_total_profile_1)
+  	@tiff = create(:user)
+		create(:user_total_profile_2)
+  end
 
-# 	context "security" do 
-# 		before(:each) do
-#       login_as @mark
-#   		visit '/user_profiles/1/edit' 
-#   		expect(current_path).to eq edit_user_profile_path(@mark)
-#   	end
 
-# 		it "Mark can't edit Tiff's profile" do 
-# 			visit '/'
-# 			click_link 'Settings'
-# 			expect(current_path).to eq edit_user_profile_path(@mark)
-# 			visit '/user_profiles/2/edit'
-# 			save_and_open_page
-# 			expect(current_path).to eq('/') 
-# 		end
-# 	end
 
-#   context "if no image is uploaded" do 
-# 	context "user should see its curent profile records" do 
-# 		before(:each) do
-#       login_as @mark
-#   		visit '/user_profile/edit' 
-#   		expect(current_path).to eq edit_user_profile_path
-#   	end
+	context "correct profile display" do 
+		before(:each) do
+      login_as @mark
+  		visit '/user_profile/edit' 
+  		expect(current_path).to eq edit_user_profile_path(@mark)
+  	end
 
-# 		it "should display user profile" do 
-#             visit '/user_profile/edit'
-#             expect(current_path).to eq edit_user_profile_path(current_user)
-#             expect(page).to have_content(@mark.user_info && @mark.user_profile)
+		it "Mark can edit his profile" do 
+			visit '/'
+			click_link 'Settings'
+			expect(current_path).to eq edit_user_profile_path(@mark)
+			visit '/user_profile/edit'
+			save_and_open_page
+			expect(current_path).to eq('/') 
+		end
+	end
+
+  context "if no image is uploaded" do 
+	context "user should see its curent profile records" do 
+		before(:each) do
+      login_as @mark
+  		visit '/user_profile/edit' 
+  		expect(current_path).to eq edit_user_profile_path
+  	end
+
+		it "should display user profile" do 
+            visit '/user_profile/edit'
+            expect(current_path).to eq edit_user_profile_path(current_user)
+            expect(page).to have_content(@mark.user_info && @mark.user_profile)
            
-# 	end
+	end
 
-  
-     
+  end
+end
 
-#   end
-# end
+  	xit "Tiff can add a photo" do
+  	end
+  end
 
-#   	xit "Tiff can add a photo" do
-#   	end
-#   end
-# end
+
