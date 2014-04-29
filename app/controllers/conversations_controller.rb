a004aeadcacd7372d458c9a1bdd9b5c5a87c98e2
Karolis Noreika <kano@kano.lt>
2014-04-29 13:59:13 +0100

    conversations controller and views

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,16 +1,45 @@
 class ConversationsController < ApplicationController
+
+  def index
+    @conversations = Conversation.all
+    @message = Message.new
+  end
+
   def new
+    @conversation = Conversation.new
   end
 
   def create
+    @conversation = Conversation.new params[:conversation].permit(:subject)
+    # if @conversation.participants = []
+    
+    @conversation.participants << Participant.new(:user => current_user, :conversation => @conversation)
+    @conversation.save
+    redirect_to '/conversations/'
+
   end
 
   def edit
   end
 
+  def show
+    @conversation = Conversation.find params[:id]
+   
+  
+  end
+
   def update
+
+
   end
 
   def destroy
+    @conversation = Conversation.find params[:id]
+    if @conversation.destroy
+      flash[:notice] = "Message deleted"
+      redirect_to '/conversations/'
+    else
+      flash[:notice] = "There was an error while deleting that message"
+    end
   end
 end
