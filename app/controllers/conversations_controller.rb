12640e3723b9d8ae939fa9f37fc62edd6ec69ab3
fred mac <mcfredmac@icloud.com>
2014-04-30 16:07:27 +0100

    messaging gem installed sort of

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -36,32 +36,33 @@ class ConversationsController < ApplicationController
 
   #########################################
 
-private
- 
-def mailbox
- @mailbox ||= current_user.mailbox
-end
- 
-def conversation
- @conversation ||= mailbox.conversations.find(params[:id])
-end
- 
-def conversation_params(*keys)
- fetch_params(:conversation, *keys)
-end
- 
-def message_params(*keys)
- fetch_params(:message, *keys)
-end
- 
-def fetch_params(key, *subkeys)
- params[key].instance_eval do
-   case subkeys.size
-   when 0 then self
-   when 1 then self[subkeys.first]
-   else subkeys.map{|k| self[k] }
+  private
+   
+  def mailbox
+   @mailbox ||= current_user.mailbox
+  end
+   
+  def conversation
+   @conversation ||= mailbox.conversations.find(params[:id])
+  end
+   
+  def conversation_params(*keys)
+   fetch_params(:conversation, *keys)
+  end
+   
+  def message_params(*keys)
+   fetch_params(:message, *keys)
+  end
+   
+  def fetch_params(key, *subkeys)
+   params[key].instance_eval do
+     case subkeys.size
+     when 0 then self
+     when 1 then self[subkeys.first]
+     else subkeys.map{|k| self[k] }
+     end
    end
- end
+  end
 end
 
 
@@ -73,4 +74,3 @@ end
 
 
 
-  
