ef2956226a3f275e42944a94dc14e1dabb1c5331
fred mac <mcfredmac@icloud.com>
2014-05-01 18:09:23 +0100

    added another message test

--- spec/features/sending_messges_feature_spec.rb
+++ spec/features/sending_messges_feature_spec.rb
@@ -21,11 +21,26 @@ describe 'Spec Helper' do
 			click_button 'Send message'
 			expect(@tiff.mailbox.inbox.last.subject).to eq "Hello"
 		end
- 
 	end
 end
 
+describe "two equal entities" do
+  before do
+    @entity1 = FactoryGirl.create(:user)
+    @entity2 = FactoryGirl.create(:user)
+  end
 
+  describe "message sending" do
+   	before do
+     @receipt1 = @entity1.send_message(@entity2,"Body","Subject")
+     @message1 = @receipt1.notification
+    end
 
-
-
+    it "should create proper message" do
+      @message1.sender.id.should == @entity1.id
+      @message1.sender.class.should == @entity1.class
+      assert @message1.body.eql?"Body"
+      assert @message1.subject.eql?"Subject"
+    end
+  end
+end
