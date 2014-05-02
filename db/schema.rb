7077a3f60ab19cd58eddf82e854196b5cd41e836
BonoboUK <mark@superius.co.uk>
2014-05-02 13:56:53 +0100

    bug fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140502125231) do
+ActiveRecord::Schema.define(version: 20140502125558) do
 
   create_table "bookings", force: true do |t|
     t.datetime "start_time"
@@ -100,7 +100,6 @@ ActiveRecord::Schema.define(version: 20140502125231) do
     t.text     "name"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "english"
   end
 
   create_table "user_infos", force: true do |t|
