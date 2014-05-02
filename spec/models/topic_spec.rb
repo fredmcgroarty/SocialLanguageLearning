9cb97b669610394f0943fc5979d326633b1bf072
fred mac <mcfredmac@icloud.com>
2014-05-02 17:13:38 +0100

    all tests fixed and passing

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 describe "Topics" do 
 
 	context "adding" do
+		before (:each) do
+			@topic_sports = create(:topic)
+		end
 
 		it "are stored in the database" do 
 			expect(Topic.last.name).to eq 'Sports'
