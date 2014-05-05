eeb860c24170306095e005b587c15738bc3cfee8
RuslanVikhor <tokvbok@yahoo.co.uk>
2014-05-05 12:17:56 +0100

    merge

--- app/controllers/user_reviews_controller.rb
+++ app/controllers/user_reviews_controller.rb
@@ -6,13 +6,16 @@ class UserReviewsController < ApplicationController
 	end
 
 	def create
-	    @user_review = UserReview.new params_permit
+	    @user_review = UserReview.new(:for_user_id => current_user, :by_user_id => current_user.id) #params_permit
 	    @user = current_user
+
 	    @user_review.save!
 
  	 end
 
  	 def params_permit
+ 	 	#for_user_id = 
+ 	 
  	 	params[:user_review].permit(:for_user_id, :by_user_id, :rev1, :rev2, :score, :topic, :completed)
  	 end
 end
