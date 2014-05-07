5915cd27f3d10392291e22e6dad0e78458c245f8
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-07 12:43:43 +0100

    fix merge bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,6 +44,14 @@ class ApplicationController < ActionController::Base
         (@average_score = @total_score.sum / @total_score.count )
       end
     end
+
+    @topic_names =[]
+    @topics = Topic.all
+    @topics.each do |topic|
+      @topic_names << topic.name
+    end  
+    @incomplete_topics = @topic_names - @completed_topics
+
   end
 
   def get_hours
--- db/migrate/20140505150953_change_topic_name_in_user_reviews.rb
+++
@@ -1,5 +0,0 @@
-class ChangeTopicNameInUserReviews < ActiveRecord::Migration
-  def change
-  	rename_column :user_reviews, :topic, :topic_id
-  end
-end
