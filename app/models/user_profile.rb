83a480c605b9b38c7b70f3222ffc7dc65e11bc33
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-07 11:33:23 +0100

    Turn off attachment content type validation

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -4,7 +4,8 @@ class UserProfile < ActiveRecord::Base
 	has_attached_file :picture, 
 		:styles => { medium: "200x250>", thumb: "100x100>", large: "600x600>"},
 		:default_url => "/pictures/:style_missing.png"
-		validates_attachment :picture, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"]}
+  do_not_validate_attachment_file_type :picture
+	# validates_attachment :picture, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"]}
 end
 
 
