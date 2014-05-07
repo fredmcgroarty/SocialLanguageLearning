bc3ed4aef4b4ef134b27364a3db64e7bc2da3c86
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-07 04:12:07 +0100

    changed method of conversion for db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505150953) do
+ActiveRecord::Schema.define(version: 20140507030417) do
 
   create_table "bookings", force: true do |t|
     t.datetime "start_time"
