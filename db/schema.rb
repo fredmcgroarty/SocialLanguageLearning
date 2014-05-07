1268acf7614c71de3ad7888e325566f229bf7c35
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-07 12:43:43 +0100

    fix merge bugs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505150953) do
+ActiveRecord::Schema.define(version: 20140505145904) do
 
   create_table "bookings", force: true do |t|
     t.datetime "start_time"
@@ -137,7 +137,7 @@ ActiveRecord::Schema.define(version: 20140505150953) do
     t.text     "rev1"
     t.text     "rev2"
     t.integer  "score"
-    t.string   "topic_id"
+    t.integer  "topic_id"
     t.boolean  "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
