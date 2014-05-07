bf9f4f561f3315ea2ea40ef0ef2c66077c3761a8
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-07 03:56:56 +0100

    removed problem migration file

--- db/migrate/20140429131306_change_language_type_to_integer.rb
+++
@@ -1,7 +0,0 @@
-class ChangeLanguageTypeToInteger < ActiveRecord::Migration
-  def change
-    change_column :user_profiles, :native_lang, :integer
-    change_column :user_profiles, :first_lang, :integer
-    change_column :user_profiles, :second_lang, :integer
-  end
-end
