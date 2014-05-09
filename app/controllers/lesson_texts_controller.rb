a9f4174ceeb366230ce3b6ce4296b68ef94f6877
tiffaniechia <tiffaniekyrachia@gmail.com>
2014-05-09 14:45:04 +0100

    webscokets confined to rooms

--- app/controllers/lesson_texts_controller.rb
+++ app/controllers/lesson_texts_controller.rb
@@ -2,7 +2,8 @@ class LessonTextsController < ApplicationController
   
   def create
     # username: current_user.email, 
-    WebsocketRails[:chat].trigger 'new', { message: params[:message], user_name: current_user.first_name }
+    channel_name = 'chat_' + params[:uid]
+    WebsocketRails[channel_name].trigger 'new', { message: params[:message], user_name: current_user.first_name }
     render nothing: true
   end
 
